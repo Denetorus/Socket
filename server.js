39508f4922c2bfc9b284008081ccd0799b1c8b21
Denetor <gfc-com@mail.ru>
2018-05-19 08:26:54 +0300

    id u

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 
-var http = require('http');
-var ServerStatic = require('node-static');
-var WS = require('ws');
+let http = require('http');
+let ServerStatic = require('node-static');
+let WS = require('ws');
 
-var clientsNow = {};
+let clientsNow = {};
 
-var WSServer = new WS.Server({
+let WSServer = new WS.Server({
     port: 8081
 });
 
 WSServer.on('connection', function(ws){
+
     let idUser = Math.random();
     clientsNow[idUser] = ws;
     console.log('New user', 'id:', idUser);
@@ -24,11 +25,12 @@ WSServer.on('connection', function(ws){
     ws.on('close', function(){
         delete clientsNow[idUser];
     });
+
 });
 
-var myServer = new ServerStatic.Server('.');
+let myServer = new ServerStatic.Server('.');
 http.createServer(function(req, res){
     myServer.serve(req,  res)
 }).listen(8080);
 
-console.log('server running');
+console.log('server is running');
